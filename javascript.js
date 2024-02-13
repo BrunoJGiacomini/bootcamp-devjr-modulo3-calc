8e3a52a5fa25d2e7a23a34b693d215c46471b680
Bruno JGiacomini <brunojunigiacomini@gmail.com>
2024-02-13 19:22:40 -0300

    closes #3 - Implementar o botao somar

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
 function calc(){
-    alert("Calc");
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+
+    var output = num1 + num2;
+
+    document.getElementById("output").value = output;
 }
